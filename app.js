63bfc980c1b6a958708a03f77f4b2e61010d8ca8
Nataliia Kirinovych <kirinovich.natasha@gmail.com>
2023-05-25 02:16:41 +0100

    UC#1 Create endpoint

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 import express from 'express';
+import axios from 'axios';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
+app.get('/api/countries', async (req, res) => {
+
+  try {
+    const response = await axios.get('https://restcountries.com/v3.1/all');
+    const data = response.data;
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch data from API' });
+  }
+});
+
+app.use((req, res, next) => {
+  res.status(404).send('Error 404: Page not found');
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send('Error 500: Internal Server Error');
 });
 
 app.listen(3000, () => {
